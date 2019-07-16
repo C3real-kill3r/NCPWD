c1e8947ad0d04a63ea795dfe50ab8f2dffd60bc1
C3real-kill3r <brybzi@gmail.com>
2019-07-16 12:59:10 +0300

    chore(email): add email template and account verifivation email
    - add base_email template
    - add activation email template
    - modify colours to match the application colour
    - add static and template directories in the settings
    
    [Maintains #167289679]

--- NCPWD/settings.py
+++ NCPWD/settings.py
@@ -111,6 +111,22 @@ REST_FRAMEWORK = {
 }
 
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -129,3 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
