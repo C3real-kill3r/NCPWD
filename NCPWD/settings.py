8d5b07b8ca67174454710e196b7a83da08e9c080
C3real-kill3r <brybzi@gmail.com>
2019-08-08 20:08:49 +0300

    refactor settings

--- NCPWD/settings.py
+++ NCPWD/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
