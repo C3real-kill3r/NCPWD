947337cc7592973623d89cffaa1d70bda371b10b
C3real-kill3r <brybzi@gmail.com>
2019-08-08 08:55:15 +0300

    refactor settings

--- NCPWD/settings.py
+++ NCPWD/settings.py
@@ -85,7 +85,8 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST")
+        'HOST': os.getenv("DB_HOST"),
+        "PORT": 5432
     }
 }
 
