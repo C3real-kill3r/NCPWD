09aec43887961aa2383ee765bf068ec0e3619b68
C3real-kill3r <brybzi@gmail.com>
2019-08-10 11:02:44 +0300

    refactor profile model

--- NCPWD/apps/user_profile/models.py
+++ NCPWD/apps/user_profile/models.py
@@ -23,16 +23,20 @@ class Profile(models.Model):
 
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    firstname = models.CharField(max_length=20, default="firstname")
-    lastname = models.CharField(max_length=20, default="lastname")
-    email = models.EmailField(default="email")
-    national_id = models.CharField(max_length=20, default="national id")
-    phone = models.CharField(max_length=15, default="phone number")
-    location = models.CharField(max_length=30, default="location")
-    nationality = models.CharField(max_length=40, default="nationality")
-    sex = models.CharField(max_length=10, default="sex", choices=sex_choices)
-    date_of_birth = models.DateField(default="1996-06-06")
+    firstname = models.CharField(max_length=20, default="firstname", null=True)
+    lastname = models.CharField(max_length=20, default="lastname", null=True)
+    email = models.EmailField(default="email", null=True)
+    national_id = models.CharField(
+        max_length=20, default="national id", null=True)
+    phone = models.CharField(max_length=15, default="phone number", null=True)
+    location = models.CharField(max_length=30, default="location", null=True)
+    nationality = models.CharField(
+        max_length=40, default="nationality", null=True)
+    sex = models.CharField(
+        max_length=10, default="sex", choices=sex_choices, null=True)
+    date_of_birth = models.DateField(default="1996-06-06", null=True)
     disability = models.CharField(
-        max_length=15, choices=disability_choices, default="ALBINISM")
+        max_length=15, choices=disability_choices,
+        default="ALBINISM", null=True)
     cause = models.CharField(
-        max_length=10, choices=cause_choices, default="BIRTH")
+        max_length=10, choices=cause_choices, default="BIRTH", null=True)
