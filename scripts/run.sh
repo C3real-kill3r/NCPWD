984c138c9bf426d9dd9ff6a3785ddadf5b5c333f
Yahya Hussein <husain.host@gmail.com>
2019-12-12 16:54:26 +0300

    static type checker

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,22 @@ if [ "$(flake8 . ; echo $? | grep 1)" ]
 then
     tput setaf 5; echo "Flake8 Linter Warning(s):"
     echo -e "${RED} $(flake8 .)\n"; tput  sgr0;
+    tput setaf 5; echo "Mypy: Static Checker Warning(s):"
+    echo -e "${RED} $(mypy .)\n"; tput  sgr0;
+
+    python manage.py runserver
+
+elif [ "$(flake8 . ; echo $? | grep 1)" ]
+then
+    tput setaf 5; echo "Flake8 Linter Warning(s):"
+    echo -e "${RED} $(flake8 .)\n"; tput  sgr0;
+     python manage.py runserver
+
+elif [ "$(mypy . ; echo $? | grep 1)" ]
+then
+    tput setaf 5; echo "Mypy: Static Checker Warning(s):"
+    echo -e "${RED} $(mypy .)\n"; tput  sgr0;
     python manage.py runserver
 else
-     python manage.py runserver
+    python manage.py runserver
 fi
